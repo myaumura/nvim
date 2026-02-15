9a8904b36af109d91ef2fcec9c9a11f08548e0a3
Kirill Gusev <myaumuraa@gmail.com>
2026-02-15 16:44:07 +0300

    Fix

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,13 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        local lspconfig = vim.lsp.config
-        lspconfig.lua_ls.setup({})
-        lspconfig.clangd.setup({
-			on_attach = function(client, bufnr)
-				client.server_capabilites.signatureHelpProvider = false
-			end
-		})
+        vim.lsp.config("lua_ls", {
+            
+        })
+        vim.lsp.enable({"lua_ls"})
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
         -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
         -- vim.keymap.set('n', 'gD', vim.lsp.buf.declarations, {})
